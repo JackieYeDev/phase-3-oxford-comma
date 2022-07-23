ccf01b09c4c1ce10b8407aa587d4d132b8827c2a
Jackie <jye0325@gmail.com>
2022-07-23 02:52:38 -0400

    commit tree 8dd2c365d248a9c81e7724e214bee6d21dd7cf99

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    string = ""
+    if array.length == 1 then string << array[0]
+    elsif array.length == 2 then string << array.join(" and ")
+    else
+        array.each_with_index do |elem, index|
+            unless index == array.length-1 then string << "#{elem}, "
+            else string << "and #{elem}" 
+            end
+        end
+    end
+    string
 end
